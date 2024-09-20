1808cd78eba36ea53efae1290e388d64387e084b
JHammer3018 <jhammer3018@gmail.com>
2024-09-20 12:02:02 -0400

    add test setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='test_function',
+   version='1.0',
+   description='A test for if i can write a setup file',
+   license = "MIT", 
+   author='JH',
+   author_email='JH@',
+   packages=['test_function']  #same as name exclude external packages as dependencies
+)
