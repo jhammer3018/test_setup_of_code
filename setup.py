214bf47b4ca7d835d69bdfa66695937350ae7e16
J. L. Hammer <jhammer3018@gmail.com>
2024-09-20 12:04:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
    description='A test for if i can write a setup file',
    license = "MIT", 
    author='JH',
-   author_email='JH@',
+   author_email='jh4189@columbia.edu',
    packages=['test_function']  #same as name exclude external packages as dependencies
 )
